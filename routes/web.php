94a1d99dc585947d8dcbdb1f1d37151b36344884
Jeff Philapy <jeff@altahost.com>
2020-12-31 12:56:58 -0500

    adding routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/contact', function () {
+    return view('contact.form');
+});
+
+
+Route::post('/contact', function (Request $request) {
+    dd($request);
 });
