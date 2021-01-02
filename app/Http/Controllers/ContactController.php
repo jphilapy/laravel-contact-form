e909b0f42890a1da3a3fc0d1fe5ffb231cb51e8c
Jeff Philapy <jeff@altahost.com>
2021-01-01 20:19:28 -0500

    added code for calling form view and began the code to handle storing data

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('contact.form');
+    }
+
+    public function store(Request $request)
+    {
+        $data = request()->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max: 255',
+            'subject' => 'required|string|max: 255',
+            'message' => 'required|string|max: 500',
+
+        ]);
+
+        Mail::to(Auth::user()->email)->send(new ContactMail());
+        return view('frontend.contact-confirm');
+    }
 }
